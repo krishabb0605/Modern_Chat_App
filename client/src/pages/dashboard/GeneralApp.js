2e1fbb30c642458733f2539afa789ee72090a6e9
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-09-25 16:54:50 +0530

    Add UI of conversation component

--- client/src/pages/dashboard/GeneralApp.js
+++ client/src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import Chats from '../../layouts/dashboard/Chats';
+import Chats from './Chats';
+import { Box, Stack } from '@mui/material';
+import Conversation from '../../components/Conversation';
+import { useTheme } from '@emotion/react';
 
 const GeneralApp = () => {
+  const theme = useTheme();
+
   return (
-    <>
+    <Stack direction='row' sx={{ width: '100%' }}>
       {/* chats */}
       <Chats />
-    </>
+
+      {/* conversaion */}
+      <Box
+        sx={{
+          height: '100%',
+          width: 'calc(100vw - 416px)',
+          backgroundColor:
+            theme.palette.mode === 'light'
+              ? '#fff'
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
