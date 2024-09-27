d01a9f53e4f872a252d04b338a129eb32bdf40ff
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-09-27 11:47:47 +0530

    Add shortcur dialouge box

---
+++ client/src/pages/dashboard/Settings.js
@@ -0,0 +1,182 @@
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import {
+  Bell,
+  CaretLeft,
+  Image,
+  Info,
+  Key,
+  Keyboard,
+  Lock,
+  Note,
+  PencilCircle,
+} from 'phosphor-react';
+import { faker } from '@faker-js/faker';
+import Shortcuts from '../../sections/Settings/Shortcuts';
+import ThemeDialouge from '../../sections/Settings/ThemeDialouge';
+
+const Settings = () => {
+  const theme = useTheme();
+
+  const [openTheme, setOpenTheme] = useState(false);
+
+  const handleOpenTheme = () => {
+    setOpenTheme(true);
+  };
+
+  const handleCloseTheme = () => {
+    setOpenTheme(false);
+  };
+  const [openShortcuts, setOpenShortcuts] = useState(false);
+
+  const handleOpenShortcuts = () => {
+    setOpenShortcuts(true);
+  };
+
+  const handleCloseShortcuts = () => {
+    setOpenShortcuts(false);
+  };
+
+  const list = [
+    {
+      key: 0,
+      icon: <Bell size={20} />,
+      title: 'Notifications',
+      onclick: () => {},
+    },
+    {
+      key: 1,
+      icon: <Lock size={20} />,
+      title: 'Privacy',
+      onclick: () => {},
+    },
+    {
+      key: 2,
+      icon: <Key size={20} />,
+      title: 'Security',
+      onclick: () => {},
+    },
+    {
+      key: 3,
+      icon: <PencilCircle size={20} />,
+      title: 'Theme',
+      onclick: handleOpenTheme,
+    },
+    {
+      key: 4,
+      icon: <Image size={20} />,
+      title: 'Chat Wallpaper',
+      onclick: () => {},
+    },
+    {
+      key: 5,
+      icon: <Note size={20} />,
+      title: 'Request Account Info',
+      onclick: () => {},
+    },
+    {
+      key: 6,
+      icon: <Keyboard size={20} />,
+      title: 'Keyboard Shortcuts',
+      onclick: handleOpenShortcuts,
+    },
+    {
+      key: 7,
+      icon: <Info size={20} />,
+      title: 'Help',
+      onclick: () => {},
+    },
+  ];
+
+  return (
+    <>
+      <Stack direction='row' width='100%'>
+        {/* Left Panel */}
+
+        <Box
+          sx={{
+            overflowY: 'auto',
+            scrollbarWidth: 'none',
+            height: '100vh',
+            width: 320,
+            backgroundColor:
+              theme.palette.mode === 'light'
+                ? '#F8FAFF'
+                : theme.palette.background,
+            boxShadow: '0px 0px 2px rgba(0,0,0,0.25)',
+          }}
+        >
+          <Stack spacing={5} p={4}>
+            {/* Header */}
+
+            <Stack direction='row' alignItems='center' spacing={3}>
+              <IconButton>
+                <CaretLeft size={24} color='#4b4b4b' />
+              </IconButton>
+
+              <Typography variant='h6'>Settings</Typography>
+            </Stack>
+
+            {/* profile section */}
+
+            <Stack direction='row' spacing={3}>
+              <Avatar
+                src={faker.image.cats()}
+                alt={faker.name.fullName()}
+                sx={{ width: 56, height: 56 }}
+              />
+
+              <Stack spacing={0.5}>
+                <Typography variant='article'>
+                  {faker.name.fullName()}
+                </Typography>
+                <Typography variant='body2'>{faker.random.word()}</Typography>
+              </Stack>
+            </Stack>
+
+            {/* List of options */}
+
+            <Stack spacing={4}>
+              {list.map(({ key, icon, title, onclick }) => {
+                return (
+                  <>
+                    <Stack
+                      onClick={onclick}
+                      spacing={2}
+                      sx={{ cursor: 'pointer' }}
+                    >
+                      <Stack direction='row' spacing={2} alignItems='center'>
+                        <IconButton>{icon}</IconButton>
+                        <Typography>{title}</Typography>
+                      </Stack>
+                      {key !== 7 && <Divider />}
+                    </Stack>
+                  </>
+                );
+              })}
+            </Stack>
+          </Stack>
+        </Box>
+
+        {/* Right Panel */}
+      </Stack>
+      {openShortcuts && (
+        <Shortcuts open={openShortcuts} handleClose={handleCloseShortcuts} />
+      )}
+
+      {openTheme && (
+        <ThemeDialouge open={openTheme} handleClose={handleCloseTheme} />
+      )}
+    </>
+  );
+};
+
+export default Settings;
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,12 +1,12 @@
-import { Suspense, lazy } from "react";
-import { Navigate, useRoutes } from "react-router-dom";
+import { Suspense, lazy } from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
 
 // layouts
-import DashboardLayout from "../layouts/dashboard";
+import DashboardLayout from '../layouts/dashboard';
 
 // config
-import { DEFAULT_PATH } from "../config";
-import LoadingScreen from "../components/LoadingScreen";
+import { DEFAULT_PATH } from '../config';
+import LoadingScreen from '../components/LoadingScreen';
 
 const Loadable = (Component) => (props) => {
   return (
@@ -19,21 +19,24 @@ const Loadable = (Component) => (props) => {
 export default function Router() {
   return useRoutes([
     {
-      path: "/",
+      path: '/',
       element: <DashboardLayout />,
       children: [
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
-        { path: "app", element: <GeneralApp /> },
-        
-        { path: "404", element: <Page404 /> },
-        { path: "*", element: <Navigate to="/404" replace /> },
+        { path: 'app', element: <GeneralApp /> },
+        { path: 'settings', element: <Settings /> },
+
+        { path: '404', element: <Page404 /> },
+        { path: '*', element: <Navigate to='/404' replace /> },
       ],
     },
-    { path: "*", element: <Navigate to="/404" replace /> },
+    { path: '*', element: <Navigate to='/404' replace /> },
   ]);
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+  lazy(() => import('../pages/dashboard/GeneralApp'))
 );
-const Page404 = Loadable(lazy(() => import("../pages/Page404")));
+
+const Settings = Loadable(lazy(() => import('../pages/dashboard/Settings')));
+const Page404 = Loadable(lazy(() => import('../pages/Page404')));
